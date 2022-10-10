55efb40629d9b86ed32599ee56c51b78ad54ccaa
Vladislav Permyakov <vladic.p@gmail.com>
2022-10-10 16:14:01 +0300

    Initial Commit

---
+++ Pusher/PusherApp.swift
@@ -0,0 +1,17 @@
+//
+//  PusherApp.swift
+//  Pusher
+//
+//  Created by Permyakov Vladislav on 10.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct PusherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
